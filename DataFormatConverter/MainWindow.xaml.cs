cc938538c9af4759d9b604b9f6943f531ecfe265
rniinim <r.niinimaki@windowslive.com>
2018-12-05 22:30:53 +0200

    Started project, added openfile

---
+++ DataFormatConverter/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace DataFormatConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Json files (.json)|*.json|Xml files (.xml)|*.xml";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                filePath = (Path.GetFileName(filename));
+
+                MainTextBlock.Text = filePath.ToString();
+            }
+        }
+    }
+}
