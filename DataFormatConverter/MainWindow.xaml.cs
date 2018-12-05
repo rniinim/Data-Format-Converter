e6a72a1ce2942cb6faddefa735a96150fba30a7f
rniinim <r.niinimaki@windowslive.com>
2018-12-06 01:59:38 +0200

    added json/xml checking and convertion

--- DataFormatConverter/MainWindow.xaml.cs
+++ DataFormatConverter/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System;
 using System.IO;
 using System.Windows;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using System.Xml;
 
 namespace DataFormatConverter
 {
@@ -14,23 +17,52 @@ namespace DataFormatConverter
         {
             InitializeComponent();
         }
+        
 
         private void ReadFileBtn_Click(object sender, RoutedEventArgs e)
         {
+            MainTextBlock.Text = "";
+            string content = "";
+            XmlDocument doc;
             var fileContent = string.Empty;
             var filePath = string.Empty;
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Json files (.json)|*.json|Xml files (.xml)|*.xml";
             openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            openFileDialog.RestoreDirectory = true;
-
+            //openFileDialog.RestoreDirectory = true;
+            string path = "";
             if (openFileDialog.ShowDialog() == true)
             {
                 foreach (string filename in openFileDialog.FileNames)
                 filePath = (Path.GetFileName(filename));
+                MainTextBlock.Text += "File opened from: \n";
+                MainTextBlock.Text += openFileDialog.FileName;
+                path = Path.GetExtension(filePath);
 
-                MainTextBlock.Text = filePath.ToString();
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog.FileName);
+                content  = (sr.ReadToEnd());
+                sr.Close();
             }
+
+            if (path == ".xml")
+            {
+                doc = new XmlDocument();
+                doc.LoadXml(content);
+                string jsonText = JsonConvert.SerializeXmlNode(doc);
+
+                MainTextBlock.Text += "\n xml file transformed to json";
+                
+            }
+            else if(path == ".json"){
+
+                doc = JsonConvert.DeserializeXmlNode(content);
+                MainTextBlock.Text += "\n json file transformed to xml";
+            }
+            else
+            {
+
+            }
+            
         }
     }
 }
